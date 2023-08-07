c7ec144053ba56085e1d5a536e267056dd2d1b8b
Andre Burgaud <andre.burgaud@gmail.com>
2023-08-06 21:54:44 -0500

    finalizing first usable version

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module showhw
 
 go 1.20
 
-require github.com/jaypipes/ghw v0.12.0
+require (
+	github.com/fatih/color v1.15.0
+	github.com/jaypipes/ghw v0.12.0
+)
 
 require (
 	github.com/StackExchange/wmi v1.2.1 // indirect
@@ -10,9 +13,11 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/jaypipes/pcidb v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	howett.net/plist v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,47 +3,65 @@ package main
 import (
 	"fmt"
 	"math"
-	"os"
 
-	//"strings"
-
+	"github.com/fatih/color"
 	"github.com/jaypipes/ghw"
 )
 
-// func product() {
-//     product, err := ghw.Product()
-//     if err != nil {
-//         fmt.Printf("Error getting product info: %v", err)
-//     }
+const banner = ` __ _                          __    __ 
+/ _\ |__   _____      __/\  /\/ / /\ \ \
+\ \| '_ \ / _ \ \ /\ / / /_/ /\ \/  \/ /
+_\ \ | | | (_) \ V  V / __  /  \  /\  /
+\__/_| |_|\___/ \_/\_/\/ /_/    \/  \/
 
-//     fmt.Printf("%v\n", product)
-// }
+`
 
-func baseboard() {
-	baseboard, err := ghw.Baseboard()
-	if err != nil {
-		fmt.Printf("Error getting baseboard info: %v", err)
-	}
+const padding = 10
 
-	fmt.Printf("%v\n", baseboard)
+const (
+	KB = iota + 1
+	MB
+	GB
+	TB
+	PB
+)
+
+func printInfo(text string) {
+	green := color.New(color.FgHiGreen, color.Bold)
+	green.Print(text)
 }
 
-func bios() {
-	bios, err := ghw.BIOS()
-	if err != nil {
-		fmt.Printf("Error getting BIOS info: %v", err)
-	}
-
-	fmt.Printf("%v\n", bios)
+func paddWithSpaces(text string, count int) string {
+	return fmt.Sprintf("%-*s", count, text)
 }
 
-func chassis() {
-	chassis, err := ghw.Chassis()
-	if err != nil {
-		fmt.Printf("Error getting chassis info: %v", err)
-	}
+func showBanner() {
+	printInfo(banner)
+}
 
-	fmt.Printf("%v\n", chassis)
+func storageUnit(unit float64) float64 {
+	return math.Pow(math.Pow(2, 10), unit)
+}
+
+func ceilUnit(s float64, unit float64) int64 {
+	return int64(math.Ceil(float64(s) / storageUnit(unit)))
+}
+
+func formatSize(size int64) string {
+	s := float64(size / 1000.0)
+	if s < storageUnit(KB) {
+		return fmt.Sprintf("%dKB", int64(math.Ceil(s)))
+	}
+	if s < storageUnit(MB) {
+		return fmt.Sprintf("%dMB", ceilUnit(s, KB))
+	}
+	if s < storageUnit(GB) {
+		return fmt.Sprintf("%dGB", ceilUnit(s, MB))
+	}
+	if s < storageUnit(TB) {
+		return fmt.Sprintf("%dTB", ceilUnit(s, GB))
+	}
+	return fmt.Sprintf("%dUNKOWN", size)
 }
 
 func gpu() {
@@ -52,105 +70,9 @@ func gpu() {
 		fmt.Printf("Error getting GPU info: %v", err)
 	}
 
-	fmt.Printf("%v\n", gpu)
-
-	for _, card := range gpu.GraphicsCards {
-		fmt.Printf(" %v\n", card)
-	}
-}
-
-func pci() {
-	pci, err := ghw.PCI()
-	if err != nil {
-		fmt.Printf("Error getting PCI info: %v", err)
-	}
-	fmt.Printf("host PCI devices:\n")
-	fmt.Println("====================================================")
-
-	for _, device := range pci.Devices {
-		vendor := device.Vendor
-		vendorName := vendor.Name
-		if len(vendor.Name) > 20 {
-			vendorName = string([]byte(vendorName)[0:17]) + "..."
-		}
-		product := device.Product
-		productName := product.Name
-		if len(product.Name) > 40 {
-			productName = string([]byte(productName)[0:37]) + "..."
-		}
-		fmt.Printf("%-12s\t%-20s\t%-40s\n", device.Address, vendorName, productName)
-	}
-
-	addr := "0000:00:00.0"
-	if len(os.Args) == 2 {
-		addr = os.Args[1]
-	}
-	fmt.Printf("PCI device information for %s\n", addr)
-	fmt.Println("====================================================")
-	deviceInfo := pci.GetDevice(addr)
-	if deviceInfo == nil {
-		fmt.Printf("could not retrieve PCI device information for %s\n", addr)
-		return
-	}
-
-	vendor := deviceInfo.Vendor
-	fmt.Printf("Vendor: %s [%s]\n", vendor.Name, vendor.ID)
-	product := deviceInfo.Product
-	fmt.Printf("Product: %s [%s]\n", product.Name, product.ID)
-	subsystem := deviceInfo.Subsystem
-	subvendor := pci.Vendors[subsystem.VendorID]
-	subvendorName := "UNKNOWN"
-	if subvendor != nil {
-		subvendorName = subvendor.Name
-	}
-	fmt.Printf("Subsystem: %s [%s] (Subvendor: %s)\n", subsystem.Name, subsystem.ID, subvendorName)
-	class := deviceInfo.Class
-	fmt.Printf("Class: %s [%s]\n", class.Name, class.ID)
-	subclass := deviceInfo.Subclass
-	fmt.Printf("Subclass: %s [%s]\n", subclass.Name, subclass.ID)
-	progIface := deviceInfo.ProgrammingInterface
-	fmt.Printf("Programming Interface: %s [%s]\n", progIface.Name, progIface.ID)
-}
-
-func network() {
-	net, err := ghw.Network()
-	if err != nil {
-		fmt.Printf("Error getting network info: %v", err)
-	}
-
-	fmt.Printf("%v\n", net)
-
-	for _, nic := range net.NICs {
-		fmt.Printf(" %v\n", nic)
-
-		enabledCaps := make([]int, 0)
-		for x, cap := range nic.Capabilities {
-			if cap.IsEnabled {
-				enabledCaps = append(enabledCaps, x)
-			}
-		}
-		if len(enabledCaps) > 0 {
-			fmt.Printf("  enabled capabilities:\n")
-			for _, x := range enabledCaps {
-				fmt.Printf("   - %s\n", nic.Capabilities[x].Name)
-			}
-		}
-	}
-}
-
-func topology() {
-	topology, err := ghw.Topology()
-	if err != nil {
-		fmt.Printf("Error getting topology info: %v", err)
-	}
-
-	fmt.Printf("%v\n", topology)
-
-	for _, node := range topology.Nodes {
-		fmt.Printf(" %v\n", node)
-		for _, cache := range node.Caches {
-			fmt.Printf("  %v\n", cache)
-		}
+	for idx, card := range gpu.GraphicsCards {
+		printInfo(paddWithSpaces(fmt.Sprintf("GPU%d", idx+1), padding))
+		fmt.Printf(": %s %s\n", card.DeviceInfo.Product.Name, card.DeviceInfo.Vendor.Name)
 	}
 }
 
@@ -160,118 +82,51 @@ func storage() {
 		fmt.Printf("Error getting block storage info: %v", err)
 	}
 
-	//fmt.Printf("%v\n", block)
-
+	idx := 1
 	for _, disk := range block.Disks {
-		//fmt.Printf(" %v\n", disk)
 		if disk.Model == "unknown" {
 			continue
 		}
-		fmt.Println(disk.Name)
-		fmt.Println(disk.DriveType)
-		fmt.Println(disk.Model)
-
-		for _, part := range disk.Partitions {
-			fmt.Printf("  %v\n", part)
-			fmt.Printf("Name: %s", part.Name)
-			fmt.Printf(" - Type: %s\n", part.Type)
-
-		}
+		printInfo(paddWithSpaces(fmt.Sprintf("Disc%d", idx), padding))
+		fmt.Printf(": %s %s %s\n", formatSize(int64(disk.SizeBytes)), disk.DriveType, disk.Model)
+		idx += 1
 	}
 }
 
-/////////////////
-
-// Product
-
 func product() {
-	title("Product")
 	product, err := ghw.Product(ghw.WithDisableWarnings())
 	if err != nil {
 		fmt.Printf("Error getting product info: %v", err)
 	}
-	fmt.Printf("Name: %s\n", product.Name)
-	fmt.Printf("Vendor: %s\n", product.Vendor)
+	printInfo(paddWithSpaces("Product", padding))
+	fmt.Printf(": %s %s\n", product.Name, product.Vendor)
 }
 
 func cpu() {
-	title("CPU")
 	cpu, err := ghw.CPU(ghw.WithDisableWarnings())
 	if err != nil {
 		fmt.Printf("Error getting CPU info: %v", err)
 	}
-
-	cpu_count := len(cpu.Processors)
-	cpu_string := "processor"
-	if cpu_count > 1 {
-		cpu_string = cpu_string + "s"
+	for idx, proc := range cpu.Processors {
+		printInfo(paddWithSpaces(fmt.Sprintf("CPU%d", idx+1), padding))
+		fmt.Printf(": %s (%d cores, %d threads)\n", proc.Model, cpu.TotalCores, cpu.TotalThreads)
 	}
-
-	fmt.Printf("%d %s, ", cpu_count, cpu_string)
-	fmt.Printf("%d cores, ", cpu.TotalCores)
-	fmt.Printf("%d threads\n", cpu.TotalThreads)
-
-	for _, proc := range cpu.Processors {
-		fmt.Printf("Vendor: %s\n", proc.Vendor)
-		fmt.Printf("Model: %s\n", proc.Model)
-	}
-}
-
-func formatSize(size int64) string {
-	s := float64(size / 1000.0)
-	if s < math.Pow(2, 10) {
-		return fmt.Sprintf("%dKB", int64(math.Ceil(s)))
-	}
-	if s < math.Pow(math.Pow(2, 10), 2) {
-		return fmt.Sprintf("%dMB", int64(math.Ceil(float64(s)/math.Pow(2, 10))))
-	}
-	if s < math.Pow(math.Pow(2, 10), 3) {
-		return fmt.Sprintf("%dGB", int64(math.Ceil(float64(s)/math.Pow(math.Pow(2, 10), 2))))
-	}
-	if s < math.Pow(math.Pow(2, 10), 4) {
-		return fmt.Sprintf("%dTB", int64(math.Ceil(float64(s)/math.Pow(math.Pow(2, 10), 3))))
-	}
-	return fmt.Sprintf("%dUNKOWN", size)
 }
 
 func memory() {
-	title("Memory")
 	memory, err := ghw.Memory(ghw.WithDisableWarnings())
 	if err != nil {
 		fmt.Printf("Error getting memory info: %v", err)
 	}
-	fmt.Printf("Memory: %s\n", formatSize(memory.TotalUsableBytes))
-}
-
-////////////////
-
-func title(text string) {
-	fmt.Printf("\n%s\n", text)
+	printInfo(paddWithSpaces("Memory", padding))
+	fmt.Printf(": %s\n", formatSize(memory.TotalUsableBytes))
 }
 
 func main() {
-
-	// title("Memory")
-	// memory()
-	// title("Storage")
-	// storage()
-	// title("Topology")
-	// topology()
-	// title("Network")
-	// network()
-	// title("PCI")
-	// pci()
-	// title("GPU")
-	// gpu()
-	// title("Chassis")
-	// chassis()
-	// title("BIOS")
-	// bios()
-	// title("Baseboard")
-	// baseboard()
-
+	showBanner()
 	product()
-	cpu()
 	memory()
+	cpu()
 	storage()
+	gpu()
 }
